9b4ddef53211fc9561159c900467ea657e3d6106
Joey <joeyrjtd@gmail.com>
2023-03-20 19:05:17 -0300

    changes

--- my_site/settings.py
+++ my_site/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+       'default': {
        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
      }
 }
 
